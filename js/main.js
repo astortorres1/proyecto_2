8aec08379619870222c8694bb8964475a8b4a327
astortorres1 <torresastor08@gmail.com>
2023-12-14 14:12:47 -0300

    Segundo commit

--- js/main.js
+++ js/main.js
@@ -1,26 +1,63 @@
-function calcularInteres() {
-    let principal = parseFloat(document.getElementById('principal').value);
-    let tasa = parseFloat(document.getElementById('tasa').value);
-    let periodos = parseInt(document.getElementById('periodos').value);
+// Objeto para almacenar la información del formulario
+const formulario = {
+    principal: document.getElementById('principal'),
+    tasa: document.getElementById('tasa'),
+    periodos: document.getElementById('periodos'),
+    resultado: document.querySelector('.resultado')
+};
 
-    if (isNaN(principal) || isNaN(tasa) || isNaN(periodos)) {
+// Función para calcular el interés con funciones de orden superior
+function calcularInteres(calculoInteres) {
+    const valores = obtenerValores();
+
+    if (valores.some(isNaN)) {
         alert('Por favor, ingrese valores numéricos válidos.');
         return;
     }
 
-    let tasaPorPeriodo = tasa / 100;
-    let resultado = document.querySelector('.resultado');
+    const { principal, tasa, periodos, resultado } = formulario;
+    const tasaPorPeriodo = tasa.value / 100;
+
     resultado.innerHTML = '';
 
-    for (let i = 1; i <= periodos; i++) {
-        let total = principal * Math.pow(1 + tasaPorPeriodo, i);
-        resultado.innerHTML += `Después de ${i} periodos, el monto total es: $${total.toFixed(2)} ARS<br>`;
-    }
+    const resultadosPorPeriodo = Array.from({ length: parseInt(periodos.value) }, (_, i) => {
+        const total = calcularTotal(principal.value, tasaPorPeriodo, i + 1, calculoInteres);
+        return `Después de ${i + 1} periodos, el monto total es: $${total.toFixed(2)} ARS`;
+    });
+
+    resultadosPorPeriodo.forEach(result => {
+        resultado.innerHTML += `${result}<br>`;
+    });
 }
 
+// Función para resetear el formulario
 function resetearFormulario() {
-    document.getElementById('principal').value = '';
-    document.getElementById('tasa').value = '';
-    document.getElementById('periodos').value = '';
-    document.querySelector('.resultado').innerHTML = '';
+    const { principal, tasa, periodos, resultado } = formulario;
+
+    principal.value = '';
+    tasa.value = '';
+    periodos.value = '';
+    resultado.innerHTML = '';
+}
+
+// Función para obtener los valores del formulario
+function obtenerValores() {
+    const { principal, tasa, periodos } = formulario;
+    return [parseFloat(principal.value), parseFloat(tasa.value), parseInt(periodos.value)];
+}
+
+// Función de orden superior para calcular el total
+function calcularTotal(principal, tasaPorPeriodo, periodo, calculoInteres) {
+    return calculoInteres(principal, tasaPorPeriodo, periodo);
+}
+
+// Función de orden superior I para calcular el total con la fórmula original
+function calcularInteresOriginal(principal, tasaPorPeriodo, periodo) {
+    return principal * Math.pow(1 + tasaPorPeriodo, periodo);
+}
+
+// Función de orden superior II para calcular el total con otra fórmula
+function calcularInteresAlternativo(principal, tasaPorPeriodo, periodo) {
+    // Implementa una fórmula alternativa según tus necesidades
+    return principal * (1 + tasaPorPeriodo * periodo);
 }
